8a3cf6ce4d94dcc01bd199c7ae5464a471506bc8
Jennifer Presto <presj521@newschool.edu>
2013-04-24 23:17:07 -0400

    Button calculates average movement for 30 seconds

--- src/testApp.h
+++ src/testApp.h
@@ -24,17 +24,29 @@ public:
 	void keyPressed(int key);
     void mousePressed(int x, int y, int button);
 
-	
+	// variables for cam and faceTracker
 	ofVideoGrabber cam;
 	ofxFaceTracker tracker;
     
+    // variables created by faceTracker
     ofPolyline faceOutline;
     ofRectangle faceRect;
     ofPoint faceCenter;
     
+    // variables controlling on-screen target
     ofImage target;
     float targetW;
     float targetH;
     
+    // button to push to start measurement
     aimButton pushToAim;
+    
+    // variables regarding actual measurement
+    void collectPoints();
+    bool measuring;
+    bool calculating;
+    void measureAim();
+    int pushTime; // time button is pushed
+    vector<float> targetX; // all target x-positions during the measured time
+    vector<float> targetY; // all target y-positions during the measured time
 };
